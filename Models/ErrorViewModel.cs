73893de6e8c5071e04a9e522d883b1aae4a45124
WallochMatt <matthewrwalloch@gmail.com>
2023-03-21 13:19:24 -0500

    feat: Initializing porject files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdRoles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
