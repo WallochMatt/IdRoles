143bfce83bfb36d50934e4b1fac93298f7fe3bda
WallochMatt <matthewrwalloch@gmail.com>
2023-03-24 14:33:54 -0500

    feat: Created Customer class

---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Claims;
+
+namespace IdRoles.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
